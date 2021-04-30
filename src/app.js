e523abd10b22693010690c507c28b3606440a240
Axelle Vanden Eynde <axelle.vandeneynde@studiohyperdrive.be>
2021-04-30 15:05:40 +0200

    fix: cors

--- src/app.js
+++ src/app.js
@@ -1,16 +1,12 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
-const routes = require('./routes/index')(app);
 
 const port = process.env.PORT || 3000;
 
-var corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200
-}
+app.use(cors())
 
-app.use(cors(corsOptions))
+const routes = require('./routes/index')(app);
 
 app.listen(port, () => {
     console.log(`Unify-back listening at port ${port}`)
