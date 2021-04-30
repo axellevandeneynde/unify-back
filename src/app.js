45837a43282e8e2bc8c77029f0a374b7db7259ad
Axelle Vanden Eynde <axelle.vandeneynde@studiohyperdrive.be>
2021-04-30 14:18:03 +0200

    feat: cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000;
+const cors = require('cors');
 const routes = require('./routes/index')(app);
+
+const port = process.env.PORT || 3000;
+
+app.use(cors())
 
 app.listen(port, () => {
     console.log(`Unify-back listening at port ${port}`)
